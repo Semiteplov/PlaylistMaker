d521f6325921b9bf9dd3253c15d7f3e3a0068644
semiteplov <semiteplov@gmail.com>
2023-07-20 18:08:55 +0300

    Add click listeners for main menu buttons

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.search).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Tapped on Search!", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<ImageView>(R.id.media).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Tapped on Media!", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<ImageView>(R.id.settings).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Tapped on Settings!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
