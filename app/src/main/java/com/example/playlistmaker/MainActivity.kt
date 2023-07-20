4d074afbe6f6dc3871cb7c041d5b09bc2d869d81
semiteplov <semiteplov@gmail.com>
2023-07-21 00:54:43 +0300

    Add menu navigation

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<ImageView>(R.id.search).setOnClickListener {
-            Toast.makeText(this@MainActivity, "Tapped on Search!", Toast.LENGTH_SHORT).show()
+        findViewById<Button>(R.id.search).setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
 
-        findViewById<ImageView>(R.id.media).setOnClickListener {
-            Toast.makeText(this@MainActivity, "Tapped on Media!", Toast.LENGTH_SHORT).show()
+        findViewById<Button>(R.id.media).setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
         }
 
-        findViewById<ImageView>(R.id.settings).setOnClickListener {
-            Toast.makeText(this@MainActivity, "Tapped on Settings!", Toast.LENGTH_SHORT).show()
+        findViewById<Button>(R.id.settings).setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
         }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        findViewById<Button>(R.id.back_button).setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+    }
+}
