4c0759e52665cbe256e3004aace0045a5d9f3164
semiteplov <semiteplov@gmail.com>
2023-08-09 00:45:29 +0300

    Implement settings buttons functionality

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,6 +14,27 @@ class SettingsActivity : AppCompatActivity() {
         findViewById<Button>(R.id.back_button).setOnClickListener {
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
+        }
+
+        findViewById<Button>(R.id.share_button).setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/?from=catalog")
+            startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+        }
+
+        findViewById<Button>(R.id.support_button).setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("slaviksis@yandex.ru"))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            startActivity(emailIntent)
+        }
+
+        findViewById<Button>(R.id.terms_button).setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(browserIntent)
         }
     }
 }
