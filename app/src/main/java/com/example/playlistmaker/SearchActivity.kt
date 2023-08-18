6f6e31d37a5adac8b205f7e5af0f015f1d6b8d41
Vyacheslav Teplov <teplov.v@cdek.ru>
2023-08-19 01:31:34 +0300

    Sprint_9/23. Add dark theme and update search screen

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -7,14 +7,29 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchEditText: EditText
+    private lateinit var clearButton: ImageButton
+    private var savedText: String = ""
+    companion object {
+        const val SEARCH_TEXT = ""
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val searchEditText = findViewById<EditText>(R.id.search)
-        val clearButton = findViewById<ImageButton>(R.id.clear_button)
+        searchEditText = findViewById(R.id.search)
+        clearButton = findViewById(R.id.clear_button)
+        val backButton = findViewById<Toolbar>(R.id.search_back_button)
+
+        backButton.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        if (savedInstanceState != null) {
+            savedText = savedInstanceState.getString((SEARCH_TEXT), "")
+            searchEditText.setText(savedText)
+        }
 
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -29,5 +44,17 @@ class SearchActivity : AppCompatActivity() {
         clearButton.setOnClickListener {
             searchEditText.text.clear()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        savedText = searchEditText.text.toString()
+        outState.putString((SEARCH_TEXT), savedText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedText = savedInstanceState.getString((SEARCH_TEXT), "")
+        searchEditText.setText(savedText)
     }
 }
