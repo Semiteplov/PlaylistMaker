9a7c6692d93076e6dbbd4773909c6a8c4afeddab
semiteplov <semiteplov@gmail.com>
2023-08-20 00:50:06 +0300

    Sprint_9/23. Update search activity: replace lateinit with nullable types, replace TextWatcher with doOnTextChanged func

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,17 +2,17 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.doOnTextChanged
 
 class SearchActivity : AppCompatActivity() {
-    private lateinit var searchEditText: EditText
-    private lateinit var clearButton: ImageButton
+    private var searchEditText: EditText? = null
+    private var clearButton: ImageButton? = null
     private var savedText: String = ""
+
     companion object {
         const val SEARCH_TEXT = ""
     }
@@ -26,35 +26,29 @@ class SearchActivity : AppCompatActivity() {
 
         backButton.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
 
-        if (savedInstanceState != null) {
-            savedText = savedInstanceState.getString((SEARCH_TEXT), "")
-            searchEditText.setText(savedText)
+        savedInstanceState?.let {
+            savedText = it.getString(SEARCH_TEXT, "")
+            searchEditText?.setText(savedText)
         }
 
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        searchEditText?.doOnTextChanged { _, _, _, count ->
+            clearButton?.visibility = if (count == 0) View.GONE else View.VISIBLE
+        }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = if (s?.isEmpty() == true) View.GONE else View.VISIBLE
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-
-        clearButton.setOnClickListener {
-            searchEditText.text.clear()
+        clearButton?.setOnClickListener {
+            searchEditText?.text?.clear()
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        savedText = searchEditText.text.toString()
-        outState.putString((SEARCH_TEXT), savedText)
+        savedText = searchEditText?.text.toString()
+        outState.putString(SEARCH_TEXT, savedText)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedText = savedInstanceState.getString((SEARCH_TEXT), "")
-        searchEditText.setText(savedText)
+        savedText = savedInstanceState.getString(SEARCH_TEXT, "")
+        searchEditText?.setText(savedText)
     }
 }
