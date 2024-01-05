d5d98d1e799811e8a71fd3bfaae5fc780fbbc310
semiteplov <semiteplov@gmail.com>
2024-01-05 18:06:30 +0300

    Sprint_16/23. Refactor MainActivity

--- app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
@@ -2,31 +2,44 @@ package com.example.playlistmaker.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityMainBinding
 import com.example.playlistmaker.media.ui.activity.MediaActivity
 import com.example.playlistmaker.search.ui.SearchActivity
 import com.example.playlistmaker.settings.ui.activity.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.search).setOnClickListener {
-            val searchIntent = Intent(this, SearchActivity::class.java)
-            startActivity(searchIntent)
+        binding.search.setOnClickListener {
+            navigateToSearch()
         }
 
-        findViewById<Button>(R.id.media).setOnClickListener {
-            val mediaIntent = Intent(this, MediaActivity::class.java)
-            startActivity(mediaIntent)
+        binding.media.setOnClickListener {
+            navigateToMedia()
         }
 
-        findViewById<Button>(R.id.settings).setOnClickListener {
-            val settingsIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(settingsIntent)
+        binding.settings.setOnClickListener {
+            navigateToSettings()
         }
+    }
+
+    private fun navigateToSearch() {
+        val searchIntent = Intent(this, SearchActivity::class.java)
+        startActivity(searchIntent)
+    }
+
+    private fun navigateToMedia() {
+        val mediaIntent = Intent(this, MediaActivity::class.java)
+        startActivity(mediaIntent)
+    }
+
+    private fun navigateToSettings() {
+        val settingsIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(settingsIntent)
     }
 }
