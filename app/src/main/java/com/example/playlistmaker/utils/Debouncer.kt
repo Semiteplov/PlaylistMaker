8143079a3d78be0ff6dc323af64a4f9bd83150cd
semiteplov <semiteplov@gmail.com>
2024-03-15 12:04:59 +0300

    Sprint_20/23. Use coroutines in Debouncer

--- app/src/main/java/com/example/playlistmaker/utils/Debouncer.kt
+++ app/src/main/java/com/example/playlistmaker/utils/Debouncer.kt
@@ -1,27 +1,37 @@
 package com.example.playlistmaker.utils
 
-import android.os.Handler
-import android.os.Looper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-object Debouncer {
+object Debouncer : CoroutineScope {
     private const val CLICK_DEBOUNCE_DELAY_MILLIS = 500L
     private const val SEARCH_DEBOUNCE_DELAY_MILLIS = 2000L
 
-    private var isClickAllowed = true
-    private val handler = Handler(Looper.getMainLooper())
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + SupervisorJob()
 
-    fun clickDebounce(): Boolean {
-        val current = isClickAllowed
-        if (isClickAllowed) {
-            isClickAllowed = false
-            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY_MILLIS)
+    private var clickJob: Job? = null
+    private var searchJob: Job? = null
+
+    fun clickDebounce(action: () -> Unit): Boolean {
+        clickJob?.cancel()
+        clickJob = launch {
+            delay(CLICK_DEBOUNCE_DELAY_MILLIS)
+            action()
         }
-        return current
+        return clickJob?.isActive == true
     }
 
     fun requestDebounce(request: () -> Unit) {
-        val runnableRequest = Runnable(request)
-        handler.removeCallbacks(runnableRequest)
-        handler.postDelayed(runnableRequest, SEARCH_DEBOUNCE_DELAY_MILLIS)
+        searchJob?.cancel()
+        searchJob = launch {
+            delay(SEARCH_DEBOUNCE_DELAY_MILLIS)
+            request()
+        }
     }
 }
